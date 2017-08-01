6c534d0cb7b5eb007725094ca6f1294be7991419
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-08-01 05:54:54 -0400

    Create .js

---
+++ .js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+	<title>Chapter 2, Example 1</title>
+	<meta charset="utf-8" />
+</head>
+<body>
+	<script>
+		var myFirstVariable;
+		
+		myFirstVariable = "Hello";
+		alert(myFirstVariable);
+		
+		myFirstVariable = 54321;
+		alert(myFirstVariable);
+	</script>
+</body>
+</html>
